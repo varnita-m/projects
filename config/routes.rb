829ab60e569a258a17d4a13dd099130220bc94ff
Varnita Mathur <149738903+varnita-m@users.noreply.github.com>
2024-12-09 00:49:17 -0500

    create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  root "articles#index"
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
+
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
